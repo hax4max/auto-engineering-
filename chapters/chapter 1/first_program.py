dd5dd58bc51b330ecd36e98768b30d46f7099399
hax4max <itslegendary1990@mail.com>
2024-03-09 10:12:56 +0200

    challenge 1

---
+++ chapters/chapter 1/first_program.py
@@ -0,0 +1,8 @@
+# 1. Ask user for their first name
+first_name=input('type your first name:)
+
+#2. Ask user for their last name
+last_name=input('type your last name: ')
+
+# 3. Print their fullname
+print(f'your full name is{first_name}{last_name}')
