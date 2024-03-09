6566f79c073ce880b7c3a43ad2cfb888d1c73d4f
hax4max <itslegendary1990@mail.com>
2024-03-09 10:22:29 +0200

    challenge 2

---
+++ chapters/chapter 1/challenge_2.py
@@ -0,0 +1,8 @@
+# 1.season
+season=input('summer')
+# 2.adjective
+adjective=input('hot')
+# 3.number
+number=input('3')
+sentece
+print(f'In a {adjective}{season}day , ive drink at least{number}beers')
